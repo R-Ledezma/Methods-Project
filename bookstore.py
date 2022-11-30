c78391370392ced25bcd45ec92d3161383d34163
bobthehotdog <119370706+bobthehotdog@users.noreply.github.com>
2022-11-29 19:54:21 -0600

    Update bookstore.py
    added try block for menu errors

--- bookstore.py
+++ bookstore.py
@@ -3,7 +3,6 @@ import sys
 from Inventory import *
 
 def main():
-    
     try:
         connection = mysql.connector.connect(
             host="localhost",
@@ -19,31 +18,33 @@ def main():
     
     inv = Inventory(connection)
     
+    
+
     cnt = 1
     while cnt == 1:
         print("Welcome to bookstore!")
         while cnt ==1:
-            acclogin = input(str('do you have an account?(Y/N)'))
-            if acclogin == 'N':
-                print("Lets make one")
-                new_uname = input(str("Enter A Unique UserName \n"))
-                new_password = input(str("Enter A Password \n" ))
-                first_name = input(str("Enter Your first name"))
-                last_name = input(str("Enter your last name"))
-                address = input(str("enter your address"))
-                payinfo = input(str("Please enter your card number"))
-            elif acclogin == "Y":
-                uname = input(str("Please enter your username"))
-                password = input(str("please enter your password"))
-                #if unname catch
-            else:   
-                print("error please try again")
-                
-                
-                             
-                
-                
-                             
+            try:
+                acclogin = input(str('do you have an account?(Y/N, or Quit to exit) '))
+                if acclogin == "quit" or acclogin =="Quit":
+                    exit()
+                    
+                elif acclogin == 'N':
+                    print("Lets make one")
+                    new_uname = input(str("Enter A Unique UserName \n"))
+                    new_password = input(str("Enter A Password \n" ))
+                    first_name = input(str("Enter Your first name"))
+                    last_name = input(str("Enter your last name"))
+                    address = input(str("enter your address"))
+                    payinfo = input(str("Please enter your card number"))
+                elif acclogin == "Y":
+                    uname = input(str("Please enter your username"))
+                    password = input(str("please enter your password"))
+                else:   
+                    print("error please try again")
+            except ValueError:
+                print('error please try again')
+                continue
                              
         
     
