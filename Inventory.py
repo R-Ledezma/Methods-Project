29f748dfdfb5bb98d6cf7a96837e8b4a94090fe6
Na7hanL <98568997+Na7hanL@users.noreply.github.com>
2022-11-29 18:31:12 -0600

    added inventory class

---
+++ Inventory.py
@@ -0,0 +1,40 @@
+import mysql.connector
+import sys
+
+
+class Inventory:
+    def __init__(self, connection):
+        self.connection = connection
+        self.cursor = connection.cursor()
+
+    def showInventory(self):
+        print("Available Books: \n")
+        self.cursor.execute("SELECT * FROM Inventory")
+        contents = self.cursor.fetchall()
+        for x in contents:
+            print("ISBN: " ,x[0], "Quantity: ", x[1], "| Price: $", x[2], "| Author: ", x[3], "| Title: ", x[4], "\n")
+
+    def addItem(self,ISBN,Quantity,Price,Author,Title):
+        details = (str(ISBN),str(Quantity), str(Price),str(Author),str(Title))
+        query = "INSERT INTO Inventory (ISBN,Quantity,Price,Author,Title) VALUES (%s, %s, %s, %s, %s)"
+        self.cursor.execute(query,details)
+        self.connection.commit()
+
+    def updateQuantity(self,ISBN,num):
+        details = (str(ISBN),)
+        query = "SELECT Quantity FROM Inventory WHERE ISBN = %s"
+
+        self.cursor.execute(query,details)
+
+        current = self.cursor.fetchall()[0][0]
+
+
+        if int(current) == 0 and num > 0:
+            print("Cannot checkout. Item in cart is out of stock")
+            return False
+        else:
+            current = int(current) - num;
+            details = (str(current), str(ISBN))
+            query = "UPDATE Inventory SET Quantity='" + str(current) + "' WHERE ISBN = '" + str(ISBN) + "'"
+            self.cursor.execute(query)
+            self.connection.commit()
